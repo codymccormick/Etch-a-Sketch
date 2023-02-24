598155d5fe3d0878add9e6f68fe1913488dd74f6
Cody McCormick <mr.codymccormick@gmail.com>
2023-02-23 22:42:58 -0600

    added button to resize/reset the board and the ability to draw on mouseover

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-// Get the container div
 const container = document.getElementById("container");
+const boardSize = document.getElementById("size");
 
-// Create the cells
-for (let i = 0; i < 256; i++) {
-	const cell = document.createElement("div");
-	cell.classList.add("cell");
-	container.appendChild(cell);
+boardSize.addEventListener("click", newBoard);
+
+let size = 16;
+
+// Create the drawing board
+function populateBoard(size) {
+  const cellSize = `calc(100% / ${size})`;
+  for (let i = 0; i < size ** 2; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+    cell.innerHTML = i + 1;
+    cell.style.flexBasis = cellSize;
+    cell.addEventListener("mouseover", () => highlight(cell));
+  }
+}
+
+populateBoard(size);
+
+//Highlight cells on mouse over
+const highlight = (cell) => (cell.style.backgroundColor = "blue");
+
+//Removes all child nodes from a parent
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+//Creates a new drawing board of user inputed size. Currently overflows if input is above 20.
+function newBoard() {
+  newSize = prompt("Enter Board Size");
+  if (newSize < 2 || newSize > 20) {
+    alert("nope");
+    return;
+  }
+  removeAllChildNodes(container);
+  populateBoard(newSize);
 }
